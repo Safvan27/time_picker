8d0c4304c3222fb0fe224d5b93b6afa1dcc1e29b
Safvan <safvanparangodath12@gmail.com>
2020-07-08 21:56:59 +0530

    commit

--- index.php
+++ index.php
@@ -27,20 +27,6 @@
         </div>
     </body>
     <script>
-    var app =angular.module("insert",[]);
-    app.controller("controller",function($scope,$http){
-        $scope.insert=function(){
-            $http.post(
-            "insert_data.php",
-            {'name':$scope.name, 'email':$scope.email, 'age':$scope.age}
-            ).success(function(data){
-                $scope.name = null;
-                $scope.email = null;
-                $scope.age = null;
-            });
-
-        }
-
-    });
+    
     
     
